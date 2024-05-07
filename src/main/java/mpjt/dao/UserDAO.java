3b10eb4a68c2cf20de0c13181b3a9eb7c5377c55
PHJ <snsn2177@naver.com>
2024-05-07 10:27:46 +0900

    유저테이블 변경

--- src/main/java/mpjt/dao/UserDAO.java
+++ src/main/java/mpjt/dao/UserDAO.java
@@ -17,6 +17,7 @@ import mpjt.dto.UserDTO;
 public class UserDAO {
 	
 	public List<UserDTO> getUsers(){
+		System.out.println("getUsers 메서드 실행");
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;		
@@ -28,7 +29,7 @@ public class UserDAO {
 			conn = JDBCConnect.getConnection();
 			
 			// 3. sql창
-			String sql = "select user_idx,user_id,user_password,user_name,user_nickname from user";
+			String sql = "select user_idx, user_id, user_password, user_name, user_gen from user";
 			pstmt = conn.prepareStatement(sql);
 			// 4. execute
 			rs = pstmt.executeQuery();
@@ -38,9 +39,10 @@ public class UserDAO {
 				String id = rs.getString("user_id");
 				String password = rs.getString("user_password");
 				String name = rs.getString("user_name");
-				String nickname = rs.getString("user_nickname");
-				UserDTO dto = new UserDTO(idx, id, password, name, nickname);
+				String gen = rs.getString("user_gen");
+				UserDTO dto = new UserDTO(idx, id, password, name, gen);
 				userList.add(dto);
+				System.out.println("getUsers 메서드 성공");
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
--- src/main/java/mpjt/dto/UserDTO.java
+++ src/main/java/mpjt/dto/UserDTO.java
@@ -5,13 +5,7 @@ public class UserDTO {
 	private String user_id;
 	private String user_password;
 	private String user_name;
-	private String user_nickname;
-	private String user_email;
-	private String user_birthday;
 	private String user_gen;
-	private String user_pnum;
-	private String user_pic;
-	private String user_favor;
 	private String user_upd;
 	private String user_regd;
 
@@ -19,12 +13,12 @@ public class UserDTO {
 
 	}
 	
-	public UserDTO(int user_idx,String user_id, String user_password, String user_name,String user_nickname) {
+	public UserDTO(int user_idx, String user_id, String user_password, String user_name, String user_gen) {
 		this.user_idx = user_idx;
 		this.user_id = user_id;
 		this.user_password = user_password;
 		this.user_name = user_name;
-		this.user_nickname = user_nickname;
+		this.user_gen = user_gen;
 	}
 	
 	public int getUser_idx() {
@@ -51,47 +45,11 @@ public class UserDTO {
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
 	}
-	public String getUser_nickname() {
-		return user_nickname;
-	}
-	public void setUser_nickname(String user_nickname) {
-		this.user_nickname = user_nickname;
-	}
-	public String getUser_email() {
-		return user_email;
-	}
-	public void setUser_email(String user_email) {
-		this.user_email = user_email;
-	}
-	public String getUser_birthday() {
-		return user_birthday;
-	}
-	public void setUser_birthday(String user_birthday) {
-		this.user_birthday = user_birthday;
-	}
 	public String getUser_gen() {
 		return user_gen;
 	}
 	public void setUser_gen(String user_gen) {
 		this.user_gen = user_gen;
-	}
-	public String getUser_pnum() {
-		return user_pnum;
-	}
-	public void setUser_pnum(String user_pnum) {
-		this.user_pnum = user_pnum;
-	}
-	public String getUser_pic() {
-		return user_pic;
-	}
-	public void setUser_pic(String user_pic) {
-		this.user_pic = user_pic;
-	}
-	public String getUser_favor() {
-		return user_favor;
-	}
-	public void setUser_favor(String user_favor) {
-		this.user_favor = user_favor;
 	}
 	public String getUser_upd() {
 		return user_upd;
